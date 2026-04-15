8d3f7160dde40bb1d47933ff457bc94f952f15bc
adambradsha <adam.bradshaw2@student.education.wa.edu.au>
2026-04-15 15:14:49 +0800

    Completed Task 5, wrote recursive triangular number function. Function correctly returns 21 for triangular(6). Added screenshot and annotations to presentation slides.

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ def startriangle(n):
         print("*" * n)
         startriangle(n-1)
 
-startriangle(5)
+# startriangle(5)
+
+def triangular(n):
+    if n == 0:
+        return 0
+    else:
+        return n + triangular(n-1)
+
+print(triangular(6))
