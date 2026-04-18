c0a6a5227e0de825a926248fe56dde03317fbe94
adambradsha <adam.bradshaw2@student.education.wa.edu.au>
2026-04-18 17:01:28 +0800

    Day 2: Working on task 7 part 2, created simpletree function with base case. Currently only draws one branch. Added screenshot into presentation.

--- main.py
+++ main.py
@@ -23,28 +23,49 @@ def fibonacci(n):
 #task6
 #print(fibonacci(10))
 
-import turtle
+#import turtle
 #create turtle screen and set background colour
-screen=turtle.Screen()
-screen.bgcolor("White")
+#screen=turtle.Screen()
+#screen.bgcolor("White")
 # create a turtle object to draw with
-pen=turtle.Turtle()
+#pen=turtle.Turtle()
 # set colour of the turtle
-pen.color("Blue")
+#pen.color("Blue")
 # set speed of the turtle
-pen.speed(2)
+#pen.speed(2)
 # draw line of length 20 pixels
-pen.forward(20)
+#pen.forward(20)
 # rotate the turtle 90 degrees to the right to
 # change the direction it’s about to draw in
-pen.right(90)
+#pen.right(90)
 # draw the other 3 sides of a rectangle
-pen.forward(40)
-pen.right(90)
-pen.forward(20)
-pen.right(90)
-pen.forward(40)
+#pen.forward(40)
+#pen.right(90)
+#pen.forward(20)
+#pen.right(90)
+#pen.forward(40)
 #hide turtle
-pen.hideturtle()
+#pen.hideturtle()
 #keep turtle window open when done
+#turtle.done()
+
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("White")
+
+pen = turtle.Turtle()
+pen.color("Blue")
+pen.speed(0)
+
+pen.left(90)
+
+def simpletree(n):
+    if n == 0:
+        return
+
+    pen.forward(50)
+
+simpletree(4)
+
 turtle.done()
