a57867a149c8f3148af9fadc9a3cf4aaf957b3a2
adambradsha <adam.bradshaw2@student.education.wa.edu.au>
2026-04-18 15:59:47 +0800

    Day 2: Completed Task 7 part 1, tested basic turtle commands and drew a rectangle. Added screenshot with notes to presentation slides.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def startriangle(n):
     if n > 0:
         print("*" * n)
         startriangle(n-1)
-
+#task4
 # startriangle(5)
 
 def triangular(n):
@@ -12,7 +12,7 @@ def triangular(n):
         return 0
     else:
         return n + triangular(n-1)
-
+#task5
 #print(triangular(6))
 
 def fibonacci(n):
@@ -20,5 +20,31 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
+#task6
+#print(fibonacci(10))
 
-print(fibonacci(10))
+import turtle
+#create turtle screen and set background colour
+screen=turtle.Screen()
+screen.bgcolor("White")
+# create a turtle object to draw with
+pen=turtle.Turtle()
+# set colour of the turtle
+pen.color("Blue")
+# set speed of the turtle
+pen.speed(2)
+# draw line of length 20 pixels
+pen.forward(20)
+# rotate the turtle 90 degrees to the right to
+# change the direction it’s about to draw in
+pen.right(90)
+# draw the other 3 sides of a rectangle
+pen.forward(40)
+pen.right(90)
+pen.forward(20)
+pen.right(90)
+pen.forward(40)
+#hide turtle
+pen.hideturtle()
+#keep turtle window open when done
+turtle.done()
