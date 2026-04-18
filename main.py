8b61796e9e203d0bc0f61c02ad762c9ba46229f4
adambradsha <adam.bradshaw2@student.education.wa.edu.au>
2026-04-18 15:45:00 +0800

    Day 2: Completed Task 6, wrote recursive Fibonacci function, got correct output of 55. Added annotated screenshot to presentation slides.

--- main.py
+++ main.py
@@ -13,4 +13,12 @@ def triangular(n):
     else:
         return n + triangular(n-1)
 
-print(triangular(6))
+#print(triangular(6))
+
+def fibonacci(n):
+    if n <= 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(10))
